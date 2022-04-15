78430f820fa4234880a63e2fc7e621a2e7c0597a
Arturas Pavilonis <arturas.pavilonis@gmail.com>
2022-04-15 10:03:49 +0300

    add changes

--- src/main/java/com/rabbitmq/stream/FirstApplication.java
+++ src/main/java/com/rabbitmq/stream/FirstApplication.java
@@ -7,82 +7,89 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 public class FirstApplication {
-  static void log(String format, Object... arguments) {
-    System.out.println(String.format(format, arguments));
-  }
 
-  public static class Publish {
+   public static class Publish {
 
-    public static void main(String[] args) throws Exception {
-      log("Connecting...");
-      try (Environment environment =
-          Environment.builder().uri("rabbitmq-stream://localhost:5552").build()) {
+      public static void main(String[] args) throws Exception {
+         log("Connecting...");
+         try (Environment environment = createEnvironment()) {
+            log("Connected");
 
-        log("Connected");
+            log("Creating stream...");
+            environment.streamCreator().stream("first-application-stream").create();
+            log("Stream created");
 
-        log("Creating stream...");
-        environment.streamCreator().stream("first-application-stream").create();
-        log("Stream created");
+            log("Creating producer...");
+            Producer producer =
+                  environment.producerBuilder().stream("first-application-stream").build();
+            log("Producer created");
 
-        log("Creating producer...");
-        Producer producer =
-            environment.producerBuilder().stream("first-application-stream").build();
-        log("Producer created");
+            long start = System.currentTimeMillis();
+            int messageCount = 1_000_000;
+            CountDownLatch confirmLatch = new CountDownLatch(messageCount);
+            log("Sending %,d messages", messageCount);
 
-        long start = System.currentTimeMillis();
-        int messageCount = 1_000_000;
-        CountDownLatch confirmLatch = new CountDownLatch(messageCount);
-        log("Sending %,d messages", messageCount);
-        IntStream.range(0, messageCount)
-            .forEach(
-                i -> {
-                  Message message =
-                      producer
-                          .messageBuilder()
-                          .properties()
-                          .creationTime(System.currentTimeMillis())
-                          .messageId(i)
-                          .messageBuilder()
-                          .addData("hello world".getBytes(StandardCharsets.UTF_8))
-                          .build();
-                  producer.send(message, confirmationStatus -> confirmLatch.countDown());
-                });
-        log("Messages sent, waiting for confirmation...");
-        boolean done = confirmLatch.await(1, TimeUnit.MINUTES);
-        log(
-            "All messages confirmed? %s (%d ms)",
-            done ? "yes" : "no", (System.currentTimeMillis() - start));
-        log("Closing environment...");
+            IntStream.range(0, messageCount).forEach(i -> {
+
+               Message message = producer.messageBuilder()
+                     .properties()
+                     .creationTime(System.currentTimeMillis())
+                     .messageId(i)
+                     .messageBuilder()
+                     .addData("hello world".getBytes(StandardCharsets.UTF_8))
+                     .build();
+
+               producer.send(message, confirmationStatus -> confirmLatch.countDown());
+            });
+            log("Messages sent, waiting for confirmation...");
+            boolean done = confirmLatch.await(1, TimeUnit.MINUTES);
+            log(
+                  "All messages confirmed? %s (%d ms)",
+                  done ? "yes" : "no", (System.currentTimeMillis() - start));
+            log("Closing environment...");
+         }
+         log("Environment closed");
       }
-      log("Environment closed");
-    }
-  }
+   }
 
-  public static class Consume {
+   public static class Consume {
 
-    public static void main(String[] args) throws Exception {
-      log("Connecting...");
-      try (Environment environment =
-          Environment.builder().uri("rabbitmq-stream://localhost:5552").build()) {
+      public static void main(String[] args) throws Exception {
+         log("Connecting...");
+         try (Environment environment = createEnvironment()) {
+            log("Connected");
 
-        log("Connected");
+            AtomicInteger messageConsumed = new AtomicInteger(0);
+            long start = System.currentTimeMillis();
 
-        AtomicInteger messageConsumed = new AtomicInteger(0);
-        long start = System.currentTimeMillis();
-        log("Start consumer...");
-        Consumer consumer =
-            environment.consumerBuilder().stream("first-application-stream")
-                .offset(OffsetSpecification.first())
-                .messageHandler((context, message) -> messageConsumed.incrementAndGet())
-                .build();
+            log("Start consumer...");
+            environment.consumerBuilder()
+                  .stream("first-application-stream")
+                  .offset(OffsetSpecification.first())
+                  .messageHandler((context, message) -> messageConsumed.incrementAndGet())
+                  .build();
 
-        Utils.waitAtMost(60, () -> messageConsumed.get() >= 1_000_000);
-        log(
-            "Consumed %,d messages in %s ms",
-            messageConsumed.get(), (System.currentTimeMillis() - start));
-        log("Closing environment...");
+            Utils.waitAtMost(65, () -> messageConsumed.get() >= 1_000_000);
+            log(
+                  "Consumed %,d messages in %s ms",
+                  messageConsumed.get(), (System.currentTimeMillis() - start));
+            log("Closing environment...");
+         }
+         log("Environment closed");
       }
-      log("Environment closed");
-    }
-  }
+   }
+
+   static void log(String format, Object... arguments) {
+      System.out.println(String.format(format, arguments));
+   }
+
+   private static Environment createEnvironment() {
+      return Environment.builder()//.uri("rabbitmq-stream://localhost:5552")
+            .host("localhost")
+            .virtualHost("my-vhost")
+            .port(5552)
+            .username("guest")
+            .password("guest")
+            .build();
+   }
 }
